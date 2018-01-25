bd74d5e1197a6b259bdb868c2797121f889c8569
Mikhail Timofeev <mikhail_timofeev@epam.com>
2018-01-25 11:37:46 +0400

    Added Native Module, fixed iOS Network issue.

--- App/general/components/debounceHandler/debounceHandler.js
+++ App/general/components/debounceHandler/debounceHandler.js
@@ -6,10 +6,3 @@ const DELAY = 500;
 export default function (func: () => void) {
   return _.debounce(func, DELAY, { leading: true, trailing: false });
 }
-export function debounceTrailigHandler(func: () => void, delay: number = DELAY) {
-  return _.debounce(func, delay, { leading: false, trailing: true });
-}
-
-export function debounceLeading(func: () => void, delay: number = DELAY) {
-  return _.debounce(func, delay, { leading: true, trailing: false });
-}
--- lib/fetcher/ios/RNFetcher.m
+++ lib/fetcher/ios/RNFetcher.m
@@ -6,58 +6,60 @@ typedef void(^RCTResponseSenderBlock)(NSArray *);
 
 RCT_EXPORT_MODULE();
 
-RCT_EXPORT_METHOD(loadLocationListFromUrl:(NSString *)stringUrl completion:(RCTResponseSenderBlock)handler)
+RCT_EXPORT_METHOD(loadLocationListFromUrl:(NSString *)stringUrl completion:(RCTResponseSenderBlock)handler error:(RCTResponseSenderBlock)errorHandler)
 {
   NSURL *url = [NSURL URLWithString:stringUrl];
   if (!url) {
-    handler(@[[NSNull null], @"Invalid url"]);
+    errorHandler(@[[NSNull null], @"Invalid url"]);
     return;
   }
   NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]];
-  [session dataTaskWithURL:url completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+  NSURLSessionDataTask *task = [session dataTaskWithURL:url completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
     if (error || !data) {
-      handler(@[[NSNull null], error.localizedDescription]);
+      errorHandler(@[[NSNull null], error.localizedDescription]);
       return;
     }
     NSError *decodingError = nil;
     NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:0 error:&decodingError];
     if (decodingError) {
-      handler(@[[NSNull null], @"Decoding error"]);
+      errorHandler(@[[NSNull null], @"Decoding error"]);
       return;
     }
     if (![json isKindOfClass:[NSDictionary class]]) {
-      handler(@[[NSNull null], @"Expected object"]);
+      errorHandler(@[[NSNull null], @"Expected object"]);
       return;
     }
     NSString *updateDate;
     if ([json[@"updated"] isKindOfClass:[NSString class]]) {
       updateDate = json[@"updated"];
     } else {
-      handler(@[[NSNull null], @"Cannot find \"updated\""]);
+      errorHandler(@[[NSNull null], @"Cannot find \"updated\""]);
       return;
     }
     NSArray *locations;
     if ([json[@"locations"] isKindOfClass:[NSArray class]]) {
       locations = json[@"locations"];
     } else {
-      handler(@[[NSNull null], @"Cannot find \"locations\""]);
+      errorHandler(@[[NSNull null], @"Cannot find \"locations\""]);
       return;
     }
     for (id location in locations) {
       if (![location isKindOfClass:[NSDictionary class]]) {
-        handler(@[[NSNull null], @"Expected objects in \"locations\""]);
+        errorHandler(@[[NSNull null], @"Expected objects in \"locations\""]);
         return;
       }
       NSDictionary *loc = location;
       if (!([loc[@"name"] isKindOfClass:[NSString class]] &&
             [loc[@"lat"] isKindOfClass:[NSNumber class]] &&
             [loc[@"lng"] isKindOfClass:[NSNumber class]])) {
-        handler(@[[NSNull null], @"Uncompleted object in \"locations\""]);
+        errorHandler(@[[NSNull null], @"Uncompleted object in \"locations\""]);
         return;
       }
     }
-    handler(@[json, [NSNull null]]);
+    NSString* result = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+    handler(@[result, [NSNull null]]);
   }];
+  [task resume];
 }
 
 @end
